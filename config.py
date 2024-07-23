6e53191a78665ad828bc093db28ae83f820e2b8f
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-07-23 14:31:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,21 @@
+import os
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7477272049:AAEKOwIvQGM-iNOmvj-WBaM1_s9LIAGEvwg")
+API_ID = int(os.environ.get("API_ID", "28243586"))
+API_HASH = os.environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+OWNER = int(os.environ.get("OWNER", "5789545553 5340652544"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "SMRenamerBot")
+
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002235661888")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002235766670"))
+
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://smkbotz:tQU9yzGSsPMJnfUi@cluster0.vyujyre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "cluster0")
+
+STRING = os.environ.get("STRING", "AQE_fF4ABqt2HW7iVQ90Wmp3Cw31dyWkrHoIcgZP68lBBGKSRq0TJIhLgv30wac1lCMUgC0r_6hXSIG5DWRfkdZ6q5Ep2OuYmzaHg8I5oLJJUoNTb30UaZ7MZw1sgYEFvuOvpInz6l4fO7L3xceWLRXsZ_lLu65Ov8iGe_NxGvwzMtTlzq-MWmeSY0fv4wYmNxVRLja3AI8T1dwfEJgkV4TWozMyGIHc7g9wa5tipwNgnKLGnG-kxJZ3ahrVDpt7iqkGThdPf6qKSDbWJHxnymGLMpnP7l7OPlroGff48jHCaQxzcjgLjRi45lcv8Luiw3pEJDDtYvt3vrmJN3tG-xzhdjdjRwAAAAFZFXRRAA")
+BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/ad48ac09b1e6f30d2dae4.jpg")
+
+
+
+
---
+++ script.py
@@ -0,0 +1,52 @@
+class script(object):
+    HELP_TXT = """<b>Hey</b> {}
+    
+<b>Here Is The Help For My Commands.</b>"""
+
+    CAPTION_TXT = """<b><u>📝  HOW TO SET CAPTION</u></b>
+
+<b>⦿ /set_caption - Use This Command To Set Your Caption</b>
+<b>⦿ /see_caption - Use This Command To See Your Caption</b>
+<b>⦿ /del_caption - Use This Command To Delete Your Caption</b>"""
+   
+    THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
+
+<b>⦿ You Can Add Custom Thumbnail Simply By Sending A Photo To Me....</b>
+
+<b>⦿ /viewthumb - Use This Command To See Your Thumbnail</b>
+<b>⦿ /delthumb - Use This Command To Delete Your Thumbnail</b>"""
+
+    ABOUT_TXT = """<b>🤖 My Name :</b> <a href='https://t.me/SMRenamer_Bot'>Rename Bot 4GB</a>
+<b>📝 Language :</b> <a href='https://python.org'>Python 3</a>
+<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram 2.0</a>
+<b>🚀 Server :</b> <a href='https://microsoft.com/'>ᴄᴏᴍᴘ 8800</a>
+<b>📢 ᴜᴘᴅᴀᴛᴇ :</b> <a href='https://t.me/unreal_X_bot'>ᴜɴʀᴇᴀʟ ʙᴏᴛ</a>
+<b>📢 ꜱᴜᴘᴘᴏʀᴛ :</b> <a href='https://t.me/unreal_X_support'>ᴜɴʀᴇᴀʟ ꜱᴜᴘᴘᴏʀᴛ</a>
+<b>🧑‍💻 Developer :</b> <a href='https://t.me/shubham_X_official'>♡ ꜱʜᴜʙʜᴀᴍ ♡</a>
+
+<b>♻️ Bot Made By :</b> @Unreal_X_Bot"""
+
+    DONATE_TXT = """
+<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+
+<b>🛍 UPI ID:</b> <code>Maurya-Shubham@fam</code> 
+
+<b>📸 Send Screenshot After Donation To Activate Your Premium Plan </b> """
+
+    ADMIN_TXT = """<b><u>🦋 ADMIN ALL COMMANDS HERE</u></b>
+
+<b>⦿ /users - Use This Command To See Total Users</b>
+<b>⦿ /allids - Use This Command To See All Users IDs</b>
+<b>⦿ /broadcast - Use This Command To Send A Message To Users</b>
+<b>⦿ /warn - Use This Command To Send A Message To A User</b>
+<b>⦿ /resetpower - Use This Command To Reset User Power</b>
+<b>⦿ /ceasepower - Use This Command To Cease User Power</b>
+<b>⦿ /addpremium - Use This Command To Add Premium To Users</b>
+<b>⦿ /restart - Use This Command To Cancel All Process And Restart The Bot</b>"""
+
+
+
+
+
